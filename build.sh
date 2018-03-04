4847fc73bd0c428560e9f1ee61663671a20def6a
yarost12 <yaro330@gmail.com>
2018-03-04 13:11:04 +0300

    build script: we actually need to pass ARCH For proper compilation Signed-off-by: yarost12 <yaro330@gmail.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ export CONFIG_FILE="w7ds_lineageos_defconfig"
 export LOCALVERSION="-D405"
 export KBUILD_BUILD_USER="ST12"
 export KBUILD_BUILD_HOST="BLR"
+export ARCH_arm="arm"
 NC='\033[0m'
 RED='\033[0;31m'
 LGR='\033[1;32m'
@@ -17,12 +18,12 @@ make_a_fucking_defconfig()
 {
 	# I FUCKING HATE YOU ALL
 	echo -e ${LGR} "############# Generating Defconfig ##############${NC}"
-	make -s O=$objdir $CONFIG_FILE -j8
+	make ARCH=${ARCH_arm} O=$objdir $CONFIG_FILE -j8
 }
 compile() 
 {
 	echo -e ${LGR} "############### Compiling kernel ################${NC}"
-	make CROSS_COMPILE=${TC_PATH}/${GCC_PREFIX} O=$objdir -j8 \
+	make ARCH=${ARCH_arm} CROSS_COMPILE=${TC_PATH}/${GCC_PREFIX} O=$objdir -j8 \
 	zImage
 }
 ramdisk() 
